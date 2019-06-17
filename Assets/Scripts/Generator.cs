4543d5a832a649ebcf7dd35955c4b0463f2cc772
Muhammad Rizal Rusdiansyah <muhammadrizal2252@gmail.com>
2019-06-17 13:34:50 +0700

    Create Project

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,181 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public int width, height;
+    public GameObject gridPrefabs;
+    public GameObject[] dots;
+    private GridHandler[,] allTiles;
+    public GameObject[,] allDots;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new GridHandler[width, height];
+        allDots = new GameObject[width, height];
+        SettingUp();
+    }
+
+    void SettingUp()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Vector2 gridPos = new Vector2(i, j);
+                GameObject gridGO = Instantiate(gridPrefabs, gridPos, Quaternion.identity) as GameObject;
+                gridGO.transform.parent = this.transform;
+                gridGO.name = "(" + i + "," + j + ")";
+
+                //dotsGenerate
+                int randomIdx = Random.Range(0, dots.Length);
+
+                int maxIterations = 0;
+                while (MatchesAt(i,j,dots[randomIdx]) && maxIterations < 100)
+                {
+                    randomIdx = Random.Range(0, dots.Length);
+                    maxIterations++;
+                }
+                maxIterations = 0;
+
+                GameObject dot = Instantiate(dots[randomIdx], gridPos, Quaternion.identity);
+                dot.transform.parent = this.transform;
+                dot.name = "(" + i + "," + j + ")";
+                allDots[i, j] = dot;
+            }
+        }
+    }
+
+    bool MatchesAt(int column, int row, GameObject dots)
+    {
+        if (column > 1 && row > 1)
+        {
+            if (allDots[column -1, row].tag == dots.tag && allDots[column - 2, row].tag == dots.tag)
+            {
+                return true;
+            }
+            if (allDots[column, row-1].tag == dots.tag && allDots[column, row-2].tag == dots.tag)
+            {
+                return true;
+            }
+        }
+        else if (column <= 1 || row <= 1)
+        {
+            if (row > 1)
+            {
+                if (allDots[column, row - 1].tag == dots.tag && allDots[column, row - 2].tag == dots.tag)
+                {
+                    return true;
+                }
+            }
+            if (column > 1)
+            {
+                if (allDots[column-1, row].tag == dots.tag && allDots[column-2, row].tag == dots.tag)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    void DestroyDotsAt(int column, int row)
+    {
+        if (allDots[column, row].GetComponent<SwipeDots>().isMatch)
+        {
+            Destroy(allDots[column, row]);
+            allDots[column, row] = null;
+        }
+    }
+
+    public void DestroyDotsGO()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (allDots[i,j] != null)
+                {
+                    DestroyDotsAt(i, j);
+                }
+            }
+        }
+        StartCoroutine(decraseRow());
+    }
+
+    IEnumerator decraseRow()
+    {
+        int nullCount = 0;
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (allDots[i,j] == null)
+                {
+                    nullCount++;
+                }
+                else if (nullCount > 0)
+                {
+                    allDots[i, j].GetComponent<SwipeDots>().row -= nullCount;
+                    allDots[i, j] = null;
+                }
+            }
+            nullCount = 0;
+        }
+        yield return new WaitForSeconds(.4f);
+
+        StartCoroutine(fillBoardCounter());
+    }
+
+    void RefillDots()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (allDots[i,j] == null)
+                {
+                    Vector2 newDotPos = new Vector2(i, j);
+                    int dotToUse = Random.Range(0, dots.Length);
+                    GameObject newDot = Instantiate(dots[dotToUse], newDotPos, Quaternion.identity);
+                    newDot.transform.parent = this.transform;
+                    allDots[i, j] = newDot;
+                }
+            }
+        }
+    }
+
+    bool MatchedOnBoard()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if (allDots[i,j] != null)
+                {
+                    if (allDots[i,j].GetComponent<SwipeDots>().isMatch)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    IEnumerator fillBoardCounter()
+    {
+        RefillDots();
+        yield return new WaitForSeconds(.5f);
+
+        while (MatchedOnBoard())
+        {
+            yield return new WaitForSeconds(.5f);
+            DestroyDotsGO();
+        }
+    }
+}
---
+++ Assets/Scripts/SwipeDots.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeDots : MonoBehaviour
+{
+    public int column, row;
+    private Generator gen;
+    private GameObject otherDot;
+    private int prevCol, prevRow;
+    private int targetX, targetY;
+    private Vector2 startTouchPos, finalTouchPos, tempPos;
+    public float angleOfTouch;
+    public bool isMatch;
+    private int swipeThreshold = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gen = FindObjectOfType<Generator>();
+        targetX = (int)transform.position.x;
+        targetY = (int)transform.position.y;
+        row = targetY;
+        column = targetX;
+        prevCol = column;
+        prevRow = row;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDots();
+        if (isMatch)
+        {
+            SpriteRenderer mySprite = GetComponent<SpriteRenderer>();
+            mySprite.color = new Color(0f, 0f, 0f, .2f);
+        }
+
+        targetX = column;
+        targetY = row;
+
+        if (Mathf.Abs(targetX - transform.position.x) > .1f)
+        {
+            tempPos = new Vector2(targetX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPos, .5f);
+            if (gen.allDots[column, row] != this.gameObject)
+            {
+                gen.allDots[column, row] = this.gameObject;
+            }
+        }
+        else
+        {
+            tempPos = new Vector2(targetX, transform.position.y);
+            transform.position = tempPos;
+        }
+        if (Mathf.Abs(targetY - transform.position.y) > .1f)
+        {
+            tempPos = new Vector2(transform.position.x, targetY);
+            transform.position = Vector2.Lerp(transform.position, tempPos, .5f);
+            if (gen.allDots[column, row] != this.gameObject)
+            {
+                gen.allDots[column, row] = this.gameObject;
+            }
+        }
+        else
+        {
+            tempPos = new Vector2(transform.position.x, targetY);
+            transform.position = tempPos;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        startTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseUp()
+    {
+        finalTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngel();
+    }
+
+    void CalculateAngel()
+    {
+        if (Mathf.Abs(finalTouchPos.y - startTouchPos.y) > swipeThreshold || Mathf.Abs(finalTouchPos.x - startTouchPos.x) > swipeThreshold)
+        {
+            angleOfTouch = Mathf.Atan2(finalTouchPos.y - startTouchPos.y, finalTouchPos.x - startTouchPos.x) * 180 / Mathf.PI;
+            MoveDots();
+        }
+    }
+
+    void MoveDots()
+    {
+        if (angleOfTouch <= 45 && angleOfTouch > -45 && column < gen.width-1)
+        {
+            otherDot = gen.allDots[column + 1, row];
+            otherDot.GetComponent<SwipeDots>().column -= 1;
+            column += 1;
+        }
+        else if (angleOfTouch > 45 && angleOfTouch <= 135 && row < gen.height-1)
+        {
+            otherDot = gen.allDots[column, row+1];
+            otherDot.GetComponent<SwipeDots>().row -= 1;
+            row += 1;
+        }
+        else if (angleOfTouch > 135 || angleOfTouch <= -135 && column > 0)
+        {
+            otherDot = gen.allDots[column -1, row];
+            otherDot.GetComponent<SwipeDots>().column += 1;
+            column -= 1;
+        }
+        else if (angleOfTouch < -45 && angleOfTouch >= -135 && row > 0)
+        {
+            otherDot = gen.allDots[column, row - 1];
+            otherDot.GetComponent<SwipeDots>().row += 1;
+            row -= 1;
+        }
+
+        StartCoroutine(reverseDots());
+    }
+
+    void CheckDots()
+    {
+        if (column > 0 && column < gen.width-1)
+        {
+            GameObject leftDotsCheck = gen.allDots[column - 1, row];
+            GameObject rightDotsCheck = gen.allDots[column + 1, row];
+            if (leftDotsCheck != null && rightDotsCheck != null)
+            {
+                if (leftDotsCheck.tag == gameObject.tag && rightDotsCheck.tag == gameObject.tag)
+                {
+                    leftDotsCheck.GetComponent<SwipeDots>().isMatch = true;
+                    rightDotsCheck.GetComponent<SwipeDots>().isMatch = true;
+                    isMatch = true;
+                }
+            }
+        }
+
+        if (row > 0 && row < gen.height - 1)
+        {
+            GameObject upDotsCheck = gen.allDots[column, row+1];
+            GameObject downDotsCheck = gen.allDots[column, row-1];
+            if (upDotsCheck != null && downDotsCheck != null)
+            {
+                if (upDotsCheck.tag == gameObject.tag && downDotsCheck.tag == gameObject.tag)
+                {
+                    upDotsCheck.GetComponent<SwipeDots>().isMatch = true;
+                    downDotsCheck.GetComponent<SwipeDots>().isMatch = true;
+                    isMatch = true;
+                }
+            }
+        }
+    }
+
+    IEnumerator reverseDots()
+    {
+        yield return new WaitForSeconds(.6f);
+        if (otherDot != null)
+        {
+            if (!isMatch && !otherDot.GetComponent<SwipeDots>().isMatch)
+            {
+                otherDot.GetComponent<SwipeDots>().row = row;
+                otherDot.GetComponent<SwipeDots>().column = column;
+                row = prevRow;
+                column = prevCol;
+            }
+            else
+            {
+                gen.DestroyDotsGO();
+            }
+            otherDot = null;
+        }
+    }
+}
